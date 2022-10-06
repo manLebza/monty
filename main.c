e7d646999c42412d5a01c9c80116950e204f503a
root <naomieloor@gmail.com>
2022-10-06 15:10:43 -0700

    doubly func file

--- main.c
+++ main.c
@@ -1,68 +1,33 @@
-#include "monty.h"
-
-global_t vglo;
-
-/**
- * free_vglo -> frees global variables
- * Return: void
- */
-
-void free_vglo(void)
-{
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
-}
-
-/**
- * start_vglo -> initialises the global variables
- * @fd: file describtors
- * Return: void
- */
-
-void start_vglo(FILE *fd)
-{
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
-}
-
-/**
- * check_input -> checks if file exists, or if it can be opened
- * @argc: arguement count
- * @argv: arguement vector
- * Return: file struct
- */
-
-FILE *check_inut(int argc, char *argv[])
-{
+void (*f)(stack_t **stack, unsigned int line_number);
 	FILE *fd;
+	size_t size = 256;
+	ssize_t nlines = 0;
+	char *lines[2] = {NULL, NULL};
 
-	if (argc == 1 || argc > 2)
+	fd = check_input(argc, argv);
+	start_vglo(fd);
+	nlines = getline(&vglo.buffer, &size, fd);
+	while (nlines != -1)
 	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		lines[0] = _strtoky(vglo.buffer, " \t\n");
+		if (lines[0] && lines[0][0] != '#')
+		{
+			f = get_opcodes(lines[0]);
+			if (!f)
+			{
+				dprintf(2, "L%u: ", vglo.cont);
+				dprintf(2, "unknown instruction %s\n", lines[0]);
+				free_vglo();
+				exit(EXIT_FAILURE);
+			}
+			vglo.arg = _strtoky(NULL, " \t\n");
+			f(&vglo.head, vglo.cont);
+		}
+		nlines = getline(&vglo.buffer, &size, fd);
+		vglo.cont++;
 	}
-	fd = fopen(argv[1], "r");
 
-	if (fd == NULL)
-	{
-		dprintf(2, "ERROR: Cant oopen file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	free_vglo();
 
-	return (fd);
+	return (0);
 }
-
-/**
- * main -> Entry point
- * @argc: arguement count
- * @argv: arguement vector
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	void (*f)(stack_t **stack, unsigned int line_number);
